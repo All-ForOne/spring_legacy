97c1b511ff33481b4a2b40efe451d113d4a2775d
All-ForOne <ddabizz@gmail.com>
2022-02-05 14:31:38 +0900

    changed configuration to java-based

---
+++ sql/create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `spring`.`menu` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `url` VARCHAR(45) NULL,
+  `top_id` VARCHAR(45) NULL,
+  `reg_dt` DATETIME NULL,
+  `mod_dt` DATETIME NULL,
+  `use_yn` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+CREATE TABLE `spring`.`menu_auth` (
+  `menu_id` INT NOT NULL,
+  `user_id` VARCHAR(100) NOT NULL,
+  `select_yn` CHAR(1) NULL,
+  `modify_yn` CHAR(1) NULL,
+  `reg_dt` DATETIME NULL,
+  `reg_id` VARCHAR(100) NULL,
+  PRIMARY KEY (`menu_id`, `user_id`));
+
+CREATE TABLE `spring`.`user` (
+  `id` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(200) NULL,
+  `name` VARCHAR(45) NULL,
+  `reg_dt` DATETIME NULL,
+  `mod_dt` DATETIME NULL,
+  PRIMARY KEY (`id`));
+
+ALTER TABLE `spring`.`menu`
+CHANGE COLUMN `id` `id` INT NOT NULL ;
+
+ALTER TABLE `spring`.`menu`
+ADD COLUMN `order` INT NULL DEFAULT 0 AFTER `use_yn`,
+CHANGE COLUMN `use_yn` `use_yn` VARCHAR(45) NULL DEFAULT 'N' ;
---
+++ src/main/java/org/allforone/AppConfig.java
@@ -0,0 +1,7 @@
+package org.allforone;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
